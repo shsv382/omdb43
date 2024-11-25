b7c4fc56737c9472b05b85b3d1afeda8c056bfad
Sergey Shabanov <shsv382@gmail.com>
2024-11-25 19:19:45 +0300

    Переделано на Кинопоиск

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
-import { useState } from 'react'
+import { APIKEY } from './env';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const URL = 'https://api.kinopoisk.dev/v1.4/movie?year=2023&genres.name=криминал'
+
+  useEffect(() => {
+    fetch(URL, {
+      headers: {
+        'X-API-KEY': APIKEY
+      }
+    })
+      .then(res => res.json())
+      .then(console.log)
+      .catch(console.log)
+  }, [URL])
+
   return (
     <div className="App">
       <header className="App-header">
